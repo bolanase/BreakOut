63816f3cf49a697fe8197274e213994ff56aea24
Tony Severino <tony@professional-painters.com>
2014-01-16 13:54:41 -0600

    Initial Commit

---
+++ Breakout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Breakout
+//
+//  Created by Anthony  Severino on 1/16/14.
+//  Copyright (c) 2014 Simple Management Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Breakout/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Breakout
+//
+//  Created by Anthony  Severino on 1/16/14.
+//  Copyright (c) 2014 Simple Management Solutions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
